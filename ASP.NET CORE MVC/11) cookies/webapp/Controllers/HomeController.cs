49c5666e14ab465e2bdecff216a73858c867550e
Hassan <hr770735@gmail.com>
2022-07-24 19:56:27 +0500

    cookie/session added

---
+++ ASP.NET CORE MVC/11) cookies/webapp/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Security.AccessControl;
+using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using webapp.Models;
+
+
+namespace webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string data = String.Empty;
+            if(HttpContext.Request.Cookies.ContainsKey("firstRequest"))  //checking if cookie exists.
+            {
+                //getting cookie
+                string yourLastTime = HttpContext.Request.Cookies["firstRequest"];
+                data = "Welcome Back, Last time you visited: " + yourLastTime;
+
+                //updating cookie
+                HttpContext.Response.Cookies.Delete("firstRequest");
+                CookieOptions option = new CookieOptions();          //for it add using Microsoft.AspNetCore.Http;
+                option.Expires = System.DateTime.Now.AddDays(1);     //Now cookie will expire after a day. if you want cookies to be saved after closing browser as well.
+                HttpContext.Response.Cookies.Append("firstRequest", System.DateTime.Now.ToString(),option);
+            }
+            else
+            {
+                //defining cookie
+                CookieOptions option = new CookieOptions();          //for it add using Microsoft.AspNetCore.Http;
+                option.Expires = System.DateTime.Now.AddDays(1);     //Now cookie will expire after a day. if you want cookies to be saved after closing browser as well.
+                data = "You visited first time. "+System.DateTime.Now.ToString();
+                HttpContext.Response.Cookies.Append("firstRequest", System.DateTime.Now.ToString(),option);
+            }
+            return View("Index",data);
+        }
+
+        public IActionResult Remove()
+        {
+            //removing cookie
+            HttpContext.Response.Cookies.Delete("firstRequest");
+            return View("Index","Cookie Removed");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET CORE MVC/12) session/webapp/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using webapp.Models;
+
+namespace webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string data = String.Empty;
+            if(HttpContext.Session.Keys.Contains("firstRequest"))  //checking if cookie exists.
+            {
+                //getting session
+                string yourLastTime = HttpContext.Session.GetString("firstRequest");
+                data = "Welcome Back, Last time you visited: " + yourLastTime;
+
+                //updating session
+                HttpContext.Session.Remove("firstRequest");
+                HttpContext.Session.SetString("firstRequest", System.DateTime.Now.ToString());
+            }
+            else
+            {
+                // defininng session.
+                string now = System.DateTime.Now.ToString();
+                data = "You visited first time. "+now;
+                // HttpContext.Session.SetInt32("intRequest", 12);
+                HttpContext.Session.SetString("firstRequest",now);
+            }
+            return View("Index",data);
+        }
+
+        public IActionResult Remove()
+        {
+            //removing session
+            HttpContext.Session.Remove("firstRequest");
+            return View("Index","Session Removed");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
